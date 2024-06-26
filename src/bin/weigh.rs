ceff4555c5b02cf54e12a16edc8b92631351a178
CM-IV <it@davidsburgers.com>
2024-06-26 13:28:57 -0500

    Scale is working in KG and LB

--- src/bin/weigh.rs
+++ src/bin/weigh.rs
@@ -1,33 +1,26 @@
 use uom::si::{
     f64::Force,
-    force::{kilogram_force, ounce_force, pound_force},
+    force::{kilogram_force, pound_force},
 };
 
 use weighty;
 
-fn pretty_print_force(f: Force) -> String {
+fn pretty_print_force(f: Force) {
     let kg = f.get::<kilogram_force>();
     let lb = f.get::<pound_force>();
-    let oz = f.get::<ounce_force>();
 
-    if f <= Force::new::<kilogram_force>(1.0) {
-        format!("{}g ({}oz)", kg * 1000.0, oz)
-    } else {
-        let lb = lb as i64;
-        let oz = oz - (lb as f64 * 16.0);
-        format!("{}kg ({}lbs {}oz)", kg, lb, oz)
-    }
+    println!("{}kg ({}lb)", kg, lb);
 }
 
 fn main() {
     let all_scales = weighty::get_all_scales();
     if all_scales.len() > 0 {
         for scale in all_scales {
-            println!("{}", pretty_print_force(scale.read().unwrap()));
+            pretty_print_force(scale.read().unwrap());
         }
     } else {
-        println!("No scales found.  Are you sure it's plugged in, on, and something you have access to?");
+        println!(
+            "No scales found.  Are you sure it's plugged in, on, and something you have access to?"
+        );
     }
 }
-
-// TODO: add some command line arguments here
--- src/lib.rs
+++ src/lib.rs
@@ -70,9 +70,20 @@ pub fn get_all_scales() -> Vec<Box<dyn ScaleDriver>> {
     let api = HidApi::new().expect("Couldn't aquire the HID API???");
     api.device_list()
         .filter_map(|info| {
+            println!("Checking device: {:?}", info);
             let vendor_id = info.vendor_id();
             let product_id = info.product_id();
-            info.open_device(&api).ok().and_then(|dev| make_driver(vendor_id, product_id, dev))
+            println!("Device vendor_id: {}, product_id: {}", vendor_id, product_id);
+            match info.open_device(&api) {
+                Ok(dev) => {
+                    println!("Successfully opened device");
+                    make_driver(vendor_id, product_id, dev)
+                }
+                Err(e) => {
+                    println!("Failed to open device: {:?}", e);
+                    None
+                }
+            }
         })
         .collect()
 }
@@ -89,6 +100,7 @@ pub fn get_scales_by_usb_id(vendor_id: u16, product_id: u16) -> Vec<Box<dyn Scal
             let vid = info.vendor_id();
             let pid = info.product_id();
             if vid != vendor_id || pid != product_id {
+                println!("NONE");
                 return None;
             }
             info.open_device(&api).ok().and_then(|dev| make_driver(vendor_id, product_id, dev))

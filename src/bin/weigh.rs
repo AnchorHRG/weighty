f767f99b317589035122be03e0ec80031b72d777
Joshua <joshwin41@gmail.com>
2024-07-01 16:20:53 -0500

    loop test

--- src/bin/weigh.rs
+++ src/bin/weigh.rs
@@ -1,3 +1,5 @@
+use std::{thread::sleep, time::Duration};
+
 use uom::si::{
     f64::Force,
     force::{kilogram_force, pound_force},
@@ -14,13 +16,17 @@ fn pretty_print_force(f: Force) {
 
 fn main() {
     let all_scales = weighty::get_all_scales();
-    if all_scales.len() > 0 {
-        for scale in all_scales {
-            pretty_print_force(scale.read().unwrap());
+
+    loop {
+        if all_scales.len() > 0 {
+            for scale in all_scales {
+                pretty_print_force(scale.read().unwrap());
+            }
+        } else {
+            println!(
+                "No scales found.  Are you sure it's plugged in, on, and something you have access to?"
+            );
         }
-    } else {
-        println!(
-            "No scales found.  Are you sure it's plugged in, on, and something you have access to?"
-        );
+        sleep(Duration::from_millis(1000.0));
     }
 }
